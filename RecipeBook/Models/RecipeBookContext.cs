444b0997c2d37c6e30294ad46dd3f302e8faa571
noahkise <noah@kisefamily.com>
2024-02-13 13:46:56 -0800

    add tags to create and details

--- RecipeBook/Models/RecipeBookContext.cs
+++ RecipeBook/Models/RecipeBookContext.cs
@@ -6,10 +6,8 @@ namespace RecipeBook.Models
     public class RecipeBookContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<Ingredient> Ingredients { get; set; }
-        public DbSet<Quantity> Quantities { get; set; }
         public DbSet<Recipe> Recipes { get; set; }
         public DbSet<Tag> Tags { get; set; }
-        public DbSet<IngredientQuantity> IngredientQuantities { get; set; }
         public DbSet<IngredientRecipe> IngredientRecipes { get; set; }
         public DbSet<RecipeTag> RecipeTags { get; set; }
 
